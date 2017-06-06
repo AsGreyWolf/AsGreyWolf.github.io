6ad24d61b447337e202bf2689b0b9bb70ea32388
Aidar Fattakhov <asschool@yandex.ru>
2017-06-06 21:30:51 +0400

    env mapping

---
+++ hdr.py
@@ -0,0 +1,20 @@
+import imageio
+import numpy as np
+
+image = np.array(imageio.imread('waterfall_Ref.exr'))
+
+image = image ** 2.2
+brightness = 0.2126 * image[..., 0] + \
+             0.7152 * image[..., 1] + \
+             0.0722 * image[..., 2]
+image[..., 0] /= brightness
+image[..., 1] /= brightness
+image[..., 2] /= brightness
+maxbr = 50
+brightness = np.log(brightness * 1000 + 1) / np.log(maxbr * 1000 + 1)
+w, h, dims = image.shape
+result = np.zeros((w, h, 4))
+result[..., 0:3] = image ** (1 / 2.2)
+result[..., 3] = brightness
+
+imageio.imwrite('output1.png', result)
